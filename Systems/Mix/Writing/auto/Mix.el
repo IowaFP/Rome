6995bc051ada1805265e221f6ed6ba8f2d20e945
Alex Hubers <ahubers15@cornellcollege.edu>
2023-11-15 13:45:45 -0600

    propositional equality rules

---
+++ Systems/Mix/Writing/auto/Mix.el
@@ -0,0 +1,35 @@
+(TeX-add-style-hook
+ "Mix"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("acmart" "authoryear" "acmsmall" "screen")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "acmart"
+    "acmart10"
+    "natbib"
+    "tikz"
+    "tikz-cd")
+   (TeX-add-symbols
+    '("Rule" 1)
+    '("J" 4)
+    "U"
+    "Refl")
+   (LaTeX-add-labels
+    "app:RO"
+    "sec:ro-syntax"
+    "fig:syntax"
+    "sec:ro-types"
+    "fig:kinding"
+    "fig:type-equiv"
+    "sec:ro-terms"
+    "fig:typing"
+    "sec:ro-minimal"
+    "fig:minimal"
+    "fig:IxRules"
+    "fig:IxDefnEq"
+    "fig:translation")
+   (LaTeX-add-bibliographies
+    "MIx"))
+ :latex)
+
