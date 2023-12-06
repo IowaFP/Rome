938c784d56eed857b54ad4d569ec8afb6b97c8da
Alex Hubers <ahubers15@cornellcollege.edu>
2023-12-06 14:44:54 -0600

    All of the uninteresting term translations

--- Systems/Mix/Writing/sty/auto/higher.el
+++ Systems/Mix/Writing/sty/auto/higher.el
@@ -3,9 +3,9 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("ulem" "normalem") ("cleveref" "capitalize" "noabbrev")))
-   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-environments-local "code")
    (add-to-list 'LaTeX-verbatim-environments-local "codef")
+   (add-to-list 'LaTeX-verbatim-environments-local "code")
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
